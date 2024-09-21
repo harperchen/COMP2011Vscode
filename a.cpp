eccd9c3d4cce6754559fada96fedbaa08d1091b3
harperchen <harperchen1110@gmail.com>
2024-09-21 12:45:42 +0800

    init

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>     /* File: while-factorial.cpp */
+using namespace std;
+/* To compute x! = x(x-1)(x-2)...1, where x is a non -ve integer */
+int main()
+{
+    double factorial = 1;
+    int number;
+    cout << "Enter a non-negative integer: ";
+    cin >> number;
+    while (number > 0)
+    {
+        factorial *= number; // Same as: factorial = factorial*number
+        --number;            // Same as: number = number-1
+    }
+    cout << factorial << endl;
+    return 0; 
+}
